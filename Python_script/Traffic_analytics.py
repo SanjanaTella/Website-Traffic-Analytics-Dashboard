9eb2c7bcbbc7b8813035e15914be1ad39c26be94
Sanjana tella <160213524+SanjanaTella@users.noreply.github.com>
2026-03-12 13:09:50 +0530

    Update Traffic_analytics.py

--- Python_script/Traffic_analytics.py
+++ Python_script/Traffic_analytics.py
@@ -7,5 +7,4 @@ df['Engagement Score'] = df['Page Views'] * df['Time on Page']
 df['User Loyalty'] = df['Previous Visits'] / (df['Previous Visits'] + 1)
 print(df.head())
 
-df.to_csv("processed_traffic_data.csv", index=False)
 print("Processing Completed")
